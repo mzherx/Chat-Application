ff4c7787d9f882560874b20cb97243ee8e873ccd
Mazher <mazheriftikhar1@gmail.com>
2025-07-09 12:51:42 +0500

    Added Cookie Parser

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes)
+app.use(cookieParser());
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
