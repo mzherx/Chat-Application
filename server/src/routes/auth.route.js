ad5cb1ed3f711627ca39a81216f357540e6633f8
Mazher <mazheriftikhar1@gmail.com>
2025-07-19 20:13:46 +0500

    Routes Setup Completed

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,13 @@ router.post('/login', login);
 router.post('/logout', logout);
 
 router.post('/onboarding', protectRoute, onboard); // Uncomment when onboarding is implemented
+
+// Check if the user is authenticated
+router.get('/me', protectRoute, async (req, res) => {
+    res.status(200).json({
+        success: true,
+        user: req.user,
+    });
+});
 
 export default router
