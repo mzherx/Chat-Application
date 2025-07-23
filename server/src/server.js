9fac8bb9870735b577cdd45db8397089a542bc0c
Mazher <mazheriftikhar1@gmail.com>
2025-07-23 21:23:53 +0500

    22 $ git status

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import { connectDB } from './lib/db.js';
@@ -12,6 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true, // allow credentials (cookies, authorization headers, etc.)
+}))
 app.use(express.json());
 app.use(cookieParser());  // 👈 Move this ABOVE the routes
 app.use('/api/auth', authRoutes);
